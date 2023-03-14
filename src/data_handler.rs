7f633e92a867af79c138c9825ca8591002798bc0
Ahmed Hagi <ahmedosman@mac.com>
2023-03-14 08:49:12 +0100

    cleared out data handler mod

--- src/data_handler.rs
+++ src/data_handler.rs
@@ -3,12 +3,6 @@ use crate::args::{TaskSubcommand, UpdateTask};
 pub fn handle_data(data_to_handle: TaskSubcommand) {
     match data_to_handle {
         TaskSubcommand::Add(task) => {
-            // TODO: find way to change AddTask struct to TodoData struct
-            // let new_task = TodoData {
-            //     ..task
-            // };
-
-            // new_task.write_data();
             let new_task = task.to_todo_data();
             new_task.write_data().expect("No data");
         },
